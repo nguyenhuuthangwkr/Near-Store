35eb65cb7d57df061d6fd02cbfe0b788bbbee7f8
nguyenhuuthangwkr <nguyenhuuthang.work@gmail.com>
2021-10-28 17:33:51 +0700

    add slide

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -15,31 +15,8 @@
 import { Context, logging, storage } from 'near-sdk-as'
 import { Product, products} from './model';
 
-const DEFAULT_MESSAGE = 'Hello'
 const INIT_ID = 0;
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
-}
-export function setGreeting(message: string): void {
-  const account_id = Context.sender
-
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
-  storage.set(account_id, message)
-
-}
 export function addProduct(name: string, description: string, brand: string, image: string, price: i32):void {
   let product_owner = Context.sender;
   let product_id = products.length + 1;
--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -10,7 +10,8 @@ export class Product {
   price: i32;
   forSale: bool;
 
-  constructor(id: i32, owner: string, name: string, description: string, brand: string, image: string, price: i32, forSale: bool) {
+  constructor(id: i32, owner: string, name: string, description: string, brand: string, 
+    image: string, price: i32, forSale: bool) {
     this.id = id;
     this.owner = owner;
     this.name = name;
@@ -21,7 +22,7 @@ export class Product {
     this.forSale = forSale;
   }
 
-}// An array that stores messages on the blockchain
+}// An array that stores products on the blockchain
 export const products = new PersistentVector<Product>("prds");
 
 
